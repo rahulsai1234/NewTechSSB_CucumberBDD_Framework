54a4fd8a5977d048f7920fc1cae24a7a182cc1d9
Rahul <rahulgadhawe01@outlook.com>
2024-03-07 14:24:35 +0530

    07/3/24 commited

--- src/test/java/com/vtiger/runner/TestRunner.java
+++ src/test/java/com/vtiger/runner/TestRunner.java
@@ -7,7 +7,7 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		
+
 		features = "src/test/resources/Features",
 		glue = "com.vtiger.Stepdefinitions",
 		plugin = { "pretty", "json:target/cucumber-reports/Cucumber.json",
